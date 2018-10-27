fc858279cc067ceeebc35d81e69aec1d4a249e7c
AisCar <aislingcarrick@yahoo.ie>
2018-10-27 13:54:16 +0100

    Make new implementation of Java Busy Beaver FYP with Maven for easier testing and tidiness

---
+++ src/main/java/BusyBeaverGenerator.java
@@ -0,0 +1,9 @@
+public class BusyBeaverGenerator {
+  /*
+  TODO:
+  Generates some number of BBTMs
+  Checks (as much as possible) that they are valid - i.e. have a halt state
+  somewhere, even if cant be sure that it is reachable
+  Passes em along to GeneticAlgorithm
+  */
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,13 @@
+public class UserInterface {
+  /*
+  UserInterface will:
+  1. Take input parameters for GeneticAlgorithm (population size, crossover rate,
+  mutation rate etc.) as well as n (number of states)
+  2. Display information about current program run
+  */
+
+  public static void main(String[] args) {
+    //TODO
+  }
+
+}
