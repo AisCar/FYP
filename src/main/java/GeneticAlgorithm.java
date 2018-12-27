4524246b30279ffd0f0f66a42f49d2d260c91135
AisCar <aislingcarrick@yahoo.ie>
2018-12-27 15:54:50 +0000

    Added more crossover and mutation functionality (figured out next generation code flow, need to implement selection before continuing)

--- src/main/java/GeneticAlgorithm.java
+++ src/main/java/GeneticAlgorithm.java
@@ -51,30 +51,33 @@ public class GeneticAlgorithm {
   /*
   Method to create next generation of Turing Machines using crossover, mutation
   */
-  private void nextGeneration(){//ArrayList<TuringMachine> population){//ArrayList<boolean[]> population){
-    ArrayList<TuringMachine> nextGeneration = new ArrayList<TuringMachine>();
-    //Sort population by fitness
-    //Add Turing Machines unmodified to the next generation
-    //Select Turing Machines for crossover
-    //perform crossover
-    //Select chromosomes for mutation
-    //Perform mutation
-    //(add each to new generation)
-    //(Also validate and repair each)
-    //Update the population in population manager (and in here)
-    this.pop.setPopulation(nextGeneration);
-    //start the next generation
+  protected void nextGeneration(){
+    //Need to implement other methods first, will look like:
+    /*
+    //Sort current population by fitness (descending)
+    Collections.sort(population);
 
+    ArrayList<TuringMachine> nextGeneration = new ArrayList<TuringMachine>();
+
+    //perform crossover and mutation
+    nextGeneration.addAll(crossover());
+    nextGeneration.addAll(mutation());
+
+    //Perform elitist selection
+    //TODO
+
+    //Update the population in population manager
+    this.pop.setPopulation(nextGeneration);
+    population = nextGeneration;
+    */
   }
 
   /*
   genetic operators
    */
 
-  private int calculateFitness(TuringMachine busyBeaver){
+  protected int calculateFitness(TuringMachine busyBeaver){
       int score = busyBeaver.getScore();
-      //insert calculations here...
-      //TODO
       /*
       Future considerations: Bringing S(n) into the fitness function instead of
       saying "Fitness = Sigma(n)"
@@ -83,7 +86,31 @@ public class GeneticAlgorithm {
       return score;
   }
 
-  private void crossover(boolean[] parent1, boolean[] parent2){
+  protected ArrayList<TuringMachine> crossover(){
+    //TODO come back to this once select is implemented
+    ArrayList<TuringMachine> machines = new ArrayList<TuringMachine>();
+    int numParents = (int)(population.size()*crossoverRate);
+    if(numParents % 2 != 0) { //need an even number of parents
+      numParents++;
+    }
+
+    ArrayList<TuringMachine> temp = new ArrayList<TuringMachine>();
+    temp.addAll(select(numParents));//Note: Not yet implemented
+    ArrayList<boolean[]> temp2 = new ArrayList<boolean[]>();
+
+    for(int i = 0; i < numParents; i += 2){
+      boolean[] parent1 = pop.toBitArray(temp.get(i));
+      boolean[] parent2 = pop.toBitArray(temp.get(i+1));
+      //temp2.addAll
+      boolean[][] children = crossoverSingle(parent1,parent2);
+      temp2.add(children[0]);
+      temp2.add(children[1]);
+    }
+
+    return machines;
+  }
+
+  protected boolean[][] crossoverSingle(boolean[] parent1, boolean[] parent2){
     if(parent1.length != parent2.length){
       //throw new Exception("Parent chromosomes have different lengths");
       //todo error handling
@@ -102,12 +129,34 @@ public class GeneticAlgorithm {
       child1[i] = parent2[i];
       child2[i] = parent1[i];
     }
-    //wait what now? Am I overwriting parents or keeping them?
-    //If keeping them, then how do I add these children to the population?
-    //TODO come back to this once you make decisions about generation management
+
+    //TODO: Validation and repairs
+
+    boolean[][] children = {child1, child2};
+    return children;
   }
 
-  protected boolean[] mutate(boolean[] parent){
+  protected ArrayList<TuringMachine> mutation(){
+    //TODO come back to this once select is implemented
+    ArrayList<TuringMachine> machines = new ArrayList<TuringMachine>();
+    //Select TuringMachines from current population using mutationRate
+    int numToMutate = (int) (population.size() * mutationRate);
+    if(numToMutate != 0){
+      ArrayList<TuringMachine> temp = select(numToMutate);//TODO implement select
+      //Translate them to bit arrays
+      ArrayList<boolean[]> bitMachines = new ArrayList<boolean[]>();
+      //mutate the bit arrays
+      for(boolean[] bitArray : bitMachines){
+        boolean[] mutatedBitArray = mutateSingle(bitArray);
+        //TODO validation - here or there?
+        TuringMachine mutatedTuringMachine = pop.toTuringMachine(mutatedBitArray);
+        machines.add(mutatedTuringMachine);
+      }
+    }
+    return machines;
+  }
+
+  protected boolean[] mutateSingle(boolean[] parent){
       int geneToMutate = (int) (Math.random() * (parent.length - 1));
       boolean[] child  = new boolean[parent.length];
       for(int i = 0; i < parent.length; i++){
@@ -117,12 +166,19 @@ public class GeneticAlgorithm {
       return child;
    }
 
+   public ArrayList<TuringMachine> select(int numToSelect){
+     ArrayList<TuringMachine> selected = new ArrayList<TuringMachine>();
+     //TODO
+
+     return selected;
+   }
+
 
    /*
    method to repair new Turing Machines that are invalid
   */
 
-   private void repair(ArrayList<boolean[]> turingMachine){//population){
+   protected void repair(ArrayList<boolean[]> turingMachine){//population){
      //TODO
      //next state num could be > num states - modify to fix
      //any other issues?
--- src/test/java/GeneticAlgorithmTest.java
+++ src/test/java/GeneticAlgorithmTest.java
@@ -1,13 +1,21 @@
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class GeneticAlgorithmTest {
+  GeneticAlgorithm ga;
+
+  @Before
+  public void initialise(){
+    ga = new GeneticAlgorithm();
+  }
 
   @Test
   public void mutationTest(){
     boolean[] chromosome = {true, false, true, false, true, false};
-    GeneticAlgorithm ga = new GeneticAlgorithm();
-    boolean[] mutatedChromosome = ga.mutate(chromosome);
+    //GeneticAlgorithm ga = new GeneticAlgorithm();
+    boolean[] mutatedChromosome = this.ga.mutateSingle(chromosome);
     //assertNotEquals(chromosome, mutatedChromosome);//This doesn't work, these are pointers
     boolean chromosomeHasChanged = false;
     for(int i = 0; i < chromosome.length; i++){
@@ -20,7 +28,50 @@ public class GeneticAlgorithmTest {
 
   @Test
   public void crossoverTest(){
-    //TODO
+    //TODO - this isnt an actual test, really
+    boolean[] parent1 = {true, true, true, true, true, true};
+    boolean[] parent2 = {false, false, false, false, false, false};
+    boolean[][] children = this.ga.crossoverSingle(parent1, parent2);
+    String p1 = "";
+    String p2 = "";
+    String c1 = "";
+    String c2 = "";
+    int crossoverPoint = -1;
+    for(int i = 0; i < parent1.length; i++){
+      p1 = p1 + (parent1[i]? "1" : "0");
+    }
+    for(int i = 0; i < parent2.length; i++){
+      p2 = p2 + (parent2[i]? "1" : "0");
+    }
+
+    boolean crossedOver = false;
+    for(int i = 0; i < parent1.length; i++){
+      c1 = c1 + (children[0][i]? "1" : "0");
+      if(!crossedOver && (children[0][i] != parent1[i])){
+        crossedOver = true;
+        crossoverPoint = i;
+      }
+    }
+
+    crossedOver = false;
+    for(int i = 0; i < parent1.length; i++){
+      c2 = c2 + (children[1][i]? "1" : "0");
+      if(!crossedOver && (children[1][i] != parent2[i])){
+        crossedOver = true;
+        assertEquals(i, crossoverPoint);
+      }
+    }
+
+    //if(p1 != null && p2 != null && c1 != null && c2 != null)
+    System.out.println("Parent Chromosomes: " + p1 + " " + p2);
+    System.out.println("Child Chromosomes: " + c1 + " " + c2);
+    assertTrue(crossedOver);
+
   }
+
+  //TODO test the other crossover and mutation methods once select is implemented
+  //TODO test and implement select and nextGeneration
+
+
 
 }
