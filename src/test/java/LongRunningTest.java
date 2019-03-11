86ac791a1e61691f7d2727da78e316635a6c9d0d
AisCar <aislingcarrick@yahoo.ie>
2019-03-11 15:31:52 +0000

    Run test on VM for 10,000,000 generations now (n = 5)

--- src/test/java/LongRunningTest.java
+++ src/test/java/LongRunningTest.java
@@ -13,7 +13,7 @@ public class LongRunningTest {
     double crossover = 0.6;
     double mutation = 0.15;
     double elitism = 0.1;
-    int numGenerations = 1;//0000000; //TODO uncomment this when you know it works
+    int numGenerations = 10000000;
     int populationSize = 150;
 
 
@@ -56,7 +56,7 @@ public class LongRunningTest {
       catch(IOException ioe) {
         System.out.println(ioe.getMessage());
         //Don't want to lose everything so print some stuff to console
-        System.out.println(numGenerations + " generations \t highest score = " + ga.getHighestScore() + "\t highest score in final generation = " + ga.getHighestScoringTM()); //Should be the same w elitism enabled though
+        System.out.println("\n" + numGenerations + " generations \t highest score = " + ga.getHighestScore() + "\t highest score in final generation = " + ga.getHighestScoringTM()); //Should be the same w elitism enabled though
         System.out.println("Final population for " + numStates + " Turing machine genetic alorithm\n");
         for(int i = 0; i < 10; i++){
           TuringMachine tm = ga.getPopulation().get(i);
