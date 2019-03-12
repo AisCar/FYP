421cbb864ea20d31b3a76aac84d6b937dc6cfd0a
AisCar <aislingcarrick@yahoo.ie>
2019-03-12 10:29:31 +0000

    Fixed errors in fitness feature tests & began S(n) fitness feature tests (do not run - they do nothing at the moment)

--- src/test/java/TestGeneticAlgorithmParameters.java
+++ src/test/java/TestGeneticAlgorithmParameters.java
@@ -83,7 +83,7 @@ public class TestGeneticAlgorithmParameters {
   public void testIncreaseMutation(){
     int numStates = 4;
     int popSize = 200;
-    double mutationRate = 0.5;
+    double mutationRate = 0.05;
     double crossoverRate = 0.7;
     double elitismRate = 0.0;
     //int numGenerations = 10000;
@@ -142,7 +142,7 @@ public class TestGeneticAlgorithmParameters {
   public void testReachabilityFitness(){
     int numStates = 4;
     int popSize = 200;
-    double mutationRate = 0.5;
+    double mutationRate = 0.05;
     double crossoverRate = 0.7;
     double elitismRate = 0.0;
     String results = "(numGenerations: " + numGenerations + ")\ncrossover\tmutation\tpopulation\thigh score overall\t" +
@@ -169,6 +169,7 @@ public class TestGeneticAlgorithmParameters {
         //TODO
       }
     }
+    writeResults("F:/4BCT/FYP/testresults/reachabilityFitnessTestResults.txt", results);
   }
   //TODO consider a test which compares time between a GA that doesnt run TMs with unreachable halt states and one that does - maybe just do this manually though
 
@@ -176,7 +177,7 @@ public class TestGeneticAlgorithmParameters {
   public void testNumHaltsFitness(){
     int numStates = 4;
     int popSize = 200;
-    double mutationRate = 0.5;
+    double mutationRate = 0.05;
     double crossoverRate = 0.7;
     double elitismRate = 0.0;
     String results = "(numGenerations: " + numGenerations + ")\ncrossover\tmutation\tpopulation\thigh score overall\t" +
@@ -202,6 +203,7 @@ public class TestGeneticAlgorithmParameters {
         //TODO
       }
     }
+    writeResults("F:/4BCT/FYP/testresults/numHaltsFitnessTestResults.txt", results);
   }
 
 
@@ -209,7 +211,7 @@ public class TestGeneticAlgorithmParameters {
   public void testStateUseFitness(){
     int numStates = 4;
     int popSize = 200;
-    double mutationRate = 0.5;
+    double mutationRate = 0.05;
     double crossoverRate = 0.7;
     double elitismRate = 0.0;
     String results = "(numGenerations: " + numGenerations + ")\ncrossover\tmutation\tpopulation\thigh score overall\t" +
@@ -236,12 +238,74 @@ public class TestGeneticAlgorithmParameters {
         //TODO
       }
     }
+    writeResults("F:/4BCT/FYP/testresults/stateUsageFitnessTestResults.txt", results);
   }
 
+  public void testPunishShiftsFitness(){
+    int numStates = 4;
+    int popSize = 200;
+    double mutationRate = 0.05;
+    double crossoverRate = 0.7;
+    double elitismRate = 0.0;
+    String results = "(numGenerations: " + numGenerations + ")\ncrossover\tmutation\tpopulation\thigh score overall\t" +
+            "high score current\t(its fitness)\thigh fitness current\t(its score)\n\n";
+    for(int i = 0; i < 3; i++) {
+      try {
+        GeneticAlgorithm ga = new GeneticAlgorithm(popSize, numStates, numGenerations, crossoverRate, mutationRate, elitismRate);
+        //ga. //TODO
+        ga.run();
+        TuringMachine fittest = ga.getPopulation().get(0);
+        TuringMachine highestScoring = ga.getHighestScoringTM();
+        int highestEverScore = ga.getHighestScore();
+        if (fittest.getScore() != highestScoring.getScore()) {
+          fittestNotHighestScoringCounter++;
+        }
+        if (highestEverScore > highestScoring.getScore()) {
+          peakScoreNotFinalScoreCounter++;
+        }
+        results = results + crossoverRate + "\t" + mutationRate + "\t" + popSize + "\t" + highestEverScore
+                + "\t" + highestScoring.getScore() + "\t" + highestScoring.getFitness() + "\t"
+                + fittest.getFitness() + "\t" + fittest.getScore() + "\n";
+      } catch (GeneticAlgorithmException gae) {
+        //TODO
+      }
+    }
 
-  /*
-  TODO: More tests
-   */
+    writeResults("F:/4BCT/FYP/testresults/punishShiftsTestResults.txt", results);
+  }
+
+  public void testRewardShiftsFitness(){
+    int numStates = 4;
+    int popSize = 200;
+    double mutationRate = 0.05;
+    double crossoverRate = 0.7;
+    double elitismRate = 0.0;
+    String results = "(numGenerations: " + numGenerations + ")\ncrossover\tmutation\tpopulation\thigh score overall\t" +
+            "high score current\t(its fitness)\thigh fitness current\t(its score)\n\n";
+    for(int i = 0; i < 3; i++) {
+      try {
+        GeneticAlgorithm ga = new GeneticAlgorithm(popSize, numStates, numGenerations, crossoverRate, mutationRate, elitismRate);
+        //ga. //TODO
+        ga.run();
+        TuringMachine fittest = ga.getPopulation().get(0);
+        TuringMachine highestScoring = ga.getHighestScoringTM();
+        int highestEverScore = ga.getHighestScore();
+        if (fittest.getScore() != highestScoring.getScore()) {
+          fittestNotHighestScoringCounter++;
+        }
+        if (highestEverScore > highestScoring.getScore()) {
+          peakScoreNotFinalScoreCounter++;
+        }
+        results = results + crossoverRate + "\t" + mutationRate + "\t" + popSize + "\t" + highestEverScore
+                + "\t" + highestScoring.getScore() + "\t" + highestScoring.getFitness() + "\t"
+                + fittest.getFitness() + "\t" + fittest.getScore() + "\n";
+      } catch (GeneticAlgorithmException gae) {
+        //TODO
+      }
+    }
+
+    writeResults("F:/4BCT/FYP/testresults/rewardShiftsTestResults.txt", results);
+  }
 
 
   /*
@@ -296,100 +360,5 @@ public class TestGeneticAlgorithmParameters {
             "\nfittestNotHighestScoringCounter = " + fittestNotHighestScoringCounter);
   }
 
-
-
-
-
-  /*
-  Old 5-state tests - it takes too long to run a 5-state Turing machine, so was only able to run these for 100
-  generations at a time. Decided it was better to test on 4-state Turing machines
-   */
-
-  /*
-  @Test
-  public void testCrossoverRates(){
-    int numStates = 5;
-    int popSize = 200;
-    double mutationRate = 0.05;
-    double[] crossoverRates = {0.2, 0.4, 0.6, 0.8};
-    int numGenerations = 100; //TODO increase? At least to 1000...
-    String results = "crossover\tmutation\tpopulation\thigh score overall\t" +
-            "high score current\t(its fitness)\thigh fitness current\t(its score)\n\n";
-
-    for(int j = 0; j < crossoverRates.length; j++){
-      double crossoverRate = crossoverRates[j];
-      results = results + runThreeTimes(popSize, numStates, numGenerations, crossoverRate, mutationRate);
-    }
-
-    try{
-      BufferedWriter writer = new BufferedWriter(new FileWriter("F:/4BCT/FYP/testresults/crossoverRateResults.txt")); //FileWriter (String name, boolean append) instead?
-      writer.write(results);
-      writer.flush();
-      writer.close();
-    }
-    catch(IOException ioe){
-      //What do I want to do if this happens? I suppose print the results string to the commandline so its not lost forever
-      System.out.println(ioe.getMessage());
-      System.out.println(results);
-    }
-  }
-
-  @Test
-  public void testMutationRates(){
-    int numStates = 5;
-    int popSize = 200;
-    double[] mutationRates = {0.0, 0.5, 0.1, 0.15, 0.2, 0.25};
-    double crossoverRate = 0.7; //TODO this is picked randomly, change to best from above?
-    int numGenerations = 100; //TODO increase? At least to 1000...
-    String results = "crossover\tmutation\tpopulation\thigh score overall\t" +
-            "high score current\t(its fitness)\thigh fitness current\t(its score)\n\n";
-
-    for(int j = 0; j < mutationRates.length; j++){
-      double mutationRate = mutationRates[j];
-      results = results + runThreeTimes(popSize, numStates, numGenerations, crossoverRate, mutationRate);
-    }
-
-    try{
-      BufferedWriter writer = new BufferedWriter(new FileWriter("F:/4BCT/FYP/testresults/mutationRateResults.txt"));
-      writer.write(results);
-      writer.flush();
-      writer.close();
-    }
-    catch(IOException ioe){
-      //What do I want to do if this happens? I suppose print the results string to the commandline so its not lost forever
-      System.out.println(ioe.getMessage());
-      System.out.println(results);
-    }
-
-  }
-
-  @Test
-  public void testPopulationSizes(){  //never finished running
-    int numStates = 5;
-    int[] popSizes = {100, 200, 300, 400, 500};
-    double mutationRate = 0.05;
-    double crossoverRate = 0.7; //TODO this is picked randomly, change to best from above?
-    int numGenerations = 100; //TODO increase? At least to 1000...
-    String results = "crossover\tmutation\tpopulation\thigh score overall\t" +
-            "high score current\t(its fitness)\thigh fitness current\t(its score)\n\n";
-
-    for(int j = 0; j < popSizes.length; j++){
-      int popSize = popSizes[j];
-      results = results + runThreeTimes(popSize, numStates, numGenerations, crossoverRate, mutationRate);
-    }
-
-    try{
-      BufferedWriter writer = new BufferedWriter(new FileWriter("F:/4BCT/FYP/testresults/populationSizeResults.txt"));
-      writer.write(results);
-      writer.flush();
-      writer.close();
-    }
-    catch(IOException ioe){
-      //What do I want to do if this happens? I suppose print the results string to the commandline so its not lost forever
-      System.out.println(ioe.getMessage());
-      System.out.println(results);
-    }
-  }
-  */
 
 }
